9563f45d2a122518491a1e072d7aafb63708deab
Sufiyan Shaik <sufiyanshaik811@gmail.com>
2023-04-19 22:54:58 +0530

    Delete week_2_4.sql

--- week_2_4.sql
+++
@@ -1,7 +0,0 @@
-SQL > DELETE FROM course WHERE course_id='CS125';
-
-	DELETE FROM course WHERE course_id='CS125'
-	*
-	ERROR at line 5:
-	ORA-02292: integrity constraint (SUFIYAN.SYS_C008267) violated - child record
-	found
